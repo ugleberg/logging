5d6a5418b981cd51571f13485844371a4b95a231
lars@ugleberg.dk <lars@ugleberg.dk>
2022-06-19 17:53:35 +0200

    Removed out-commented code.

--- Console.cs
+++ Console.cs
@@ -25,43 +25,6 @@ public static class Console
         System.Console.WriteLine( $"{timestamp}{message}" );
     }
 
-/*
-    private static string ReadPassword()
-    {
-        var password = "";
-        var info = System.Console.ReadKey( true );
-        while( info.Key != ConsoleKey.Enter )
-        {
-            if( info.Key != ConsoleKey.Backspace )
-            {
-                System.Console.Write( "*" );
-                password += info.KeyChar;
-            }
-            else if( info.Key == ConsoleKey.Backspace )
-            {
-                if( !string.IsNullOrEmpty( password ) )
-                {
-                    // remove one character from the list of password characters
-                    password = password.Substring( 0, password.Length - 1 );
-                    // get the location of the cursor
-                    var pos = System.Console.CursorLeft;
-                    // move the cursor to the left by one character
-                    System.Console.SetCursorPosition( pos - 1, System.Console.CursorTop );
-                    // replace it with space
-                    System.Console.Write( " " );
-                    // move the cursor to the left by one character again
-                    System.Console.SetCursorPosition( pos - 1, System.Console.CursorTop );
-                }
-            }
-
-            info = System.Console.ReadKey( true );
-        }
-
-        // add a new line because user pressed enter at the end of their password
-        System.Console.WriteLine();
-        return password;
-    }
-*/
 
     public static void Read( string message, out string value )
     {
